8f99be089dd58f90a29747a16b7588de7482c49b
sahil sharma <sahilsharma@sahils-MacBook-Air.local>
2026-08-18 17:51:31 +0530

    refactor: completely delete floating right action sidebar (SocialSidebar) and its CSS styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,7 +14,6 @@ import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { Preloader } from "@/components/Preloader";
-import { SocialSidebar } from "@/components/SocialSidebar";
 
 function NotFoundComponent() {
   return (
@@ -146,7 +145,6 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       <Preloader />
       <Navbar />
-      <SocialSidebar />
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
       <Footer />
